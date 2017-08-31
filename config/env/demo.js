0cb2aaa8526e454648cf38a2519528c1a6cc60cd
yanglei <lei.yang@gi-de.com>
2017-08-31 12:21:03 +0800

    support session check in upload event

--- config/env/demo.js
+++ config/env/demo.js
@@ -1,29 +1,36 @@
 //here we will configure the port and logging for the application
 var local = {
-		"port": 20375,
-		"greeting": "Local Environment",
-		"loglevel": {
-			"level": "info",
-			"stdout": false
-		},
-		"cloud" : {
+    "port": 20375,
+    "greeting": "Local Environment",
+    "loglevel": {
+        "level": "info",
+        "stdout": false
+    },
+    "cloud": {
 
-		},
-		"eventbus": "http://127.0.0.1:20081/eventbus/",
-		"app_id": "appworld-gdsport",
-		"master_key": "XXXXXX",
-		"mount_path" : "/parse",
-		"server_url": "http://127.0.0.1:20375/parse",
-		"dashboard_users":[
-			{
-        	"user": "test",
-        	"pass": "111111"
-			}, 
-			{
-        	"user": "user2",
-        	"pass": "pass"
-			}
-		]
+    },
+    "eventbus": "http://127.0.0.1:20081/eventbus/",
+    "app_id": "appworld-gdsport",
+    "master_key": "XXXXXX",
+    "mount_path": "/parse",
+    "server_url": "http://127.0.0.1:20375/parse",
+    "dashboard_users": [{
+            "user": "test",
+            "pass": "111111"
+        },
+        {
+            "user": "user2",
+            "pass": "pass"
+        }
+    ],
+    "parse_keys": {
+        "applicationId": "appworld-gdsport",
+        "clientKey": "XXXXXX"
+    },
+    "gdsport_keys": {
+        "applicationId": "appworld-gdsport",
+        "apiKey": "XXXXXX"
+    }
 }
 
 module.exports = local;
